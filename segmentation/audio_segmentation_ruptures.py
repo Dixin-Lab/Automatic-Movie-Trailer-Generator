0fe9a20e38a691238351247ea14a8604eb585c6f
Yutong Wang <55480809+hhhh1138@users.noreply.github.com>
2024-08-23 15:56:22 +0800

    Update audio_segmentation_ruptures.py

--- segmentation/audio_segmentation_ruptures.py
+++ segmentation/audio_segmentation_ruptures.py
@@ -1,60 +1,51 @@
-import matplotlib.pyplot as plt
 import librosa
 import numpy as np
 from IPython.display import Audio, display
-import os
+import os 
+import os.path as osp 
+import argparse 
 import ruptures as rpt
 import json
-import os
-# os.environ["CUDA_VISIBLE_DEVICES"] = x
 
 # This code is modified based on the Ruptures code from https://github.com/deepcharles/ruptures.
 
-def fig_ax(figsize=(30, 5), dpi=150):
-    """Return a (matplotlib) figure and ax objects with given size."""
-    return plt.subplots(figsize=figsize, dpi=dpi)
+# segment the input music into multiple shots based on change points through ruptures. 
 
-audio_file_path = ''  # music data path
-save_result_base = ''  # save segmentation result
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser() 
+    parser.add_argument("--input_audio_path", type=str)
+    parser.add_argument("--save_audio_dir", type=str, default='./')
 
-audio_path = os.path.join(audio_file_path)
-signal, sampling_rate = librosa.load(audio_path)
+    audio = args.input_audio_path.split('/')[-1]
+    audio_name = audio[:-4] # remove the ext .mp4 
+    save_result_path = osp.join(args.save_scene_dir, audio_name+'.json')    # save segmentation result
 
-# Compute the onset strength
-hop_length_tempo = 256
-oenv = librosa.onset.onset_strength(y=signal, sr=sampling_rate, hop_length=hop_length_tempo)
+    signal, sampling_rate = librosa.load(args.input_audio_path)
 
-# Compute the tempogram
-tempogram = librosa.feature.tempogram(
-    onset_envelope=oenv,
-    sr=sampling_rate,
-    hop_length=hop_length_tempo,
-)
+    # Compute the onset strength
+    hop_length_tempo = 256
+    oenv = librosa.onset.onset_strength(y=signal, sr=sampling_rate, hop_length=hop_length_tempo)
 
-# choose the segmentation model
-algo = rpt.KernelCPD(kernel="linear").fit(tempogram.T)
+    # Compute the tempogram
+    tempogram = librosa.feature.tempogram(
+        onset_envelope=oenv,
+        sr=sampling_rate,
+        hop_length=hop_length_tempo,
+    )
 
-# Segmentation
-duration = librosa.get_duration(filename=audio_path)
-n_bkps = int(duration / 2) - 1
+    # choose the segmentation model
+    algo = rpt.KernelCPD(kernel="linear").fit(tempogram.T)
 
-bkps = algo.predict(n_bkps=n_bkps)
-# Convert the estimated change points (frame counts) to actual timestamps
-bkps_times = librosa.frames_to_time(bkps, sr=sampling_rate, hop_length=hop_length_tempo)
+    # Segmentation
+    duration = librosa.get_duration(filename=audio_path)
+    n_bkps = int(duration / 2) - 1
 
-# Displaying results
-fig, ax = fig_ax()
-_ = librosa.display.specshow(
-    tempogram,
-    ax=ax,
-    x_axis="s",
-    y_axis="tempo",
-    hop_length=hop_length_tempo,
-    sr=sampling_rate,
-)
-    
-bkps_times_list = bkps_times.tolist()
-print("audio segmentation is : {}".format(index, bkps_times_list))
+    bkps = algo.predict(n_bkps=n_bkps)
+    # Convert the estimated change points (frame counts) to actual timestamps
+    bkps_times = librosa.frames_to_time(bkps, sr=sampling_rate, hop_length=hop_length_tempo)
+        
+    bkps_times_list = bkps_times.tolist()
+    #print("audio segmentation is : {}".format(index, bkps_times_list))
 
-with open(save_result_base, 'w') as f:
-    json.dump(bkps_times_list, f)
+    with open(save_result_path, 'w') as f:
+        json.dump(bkps_times_list, f)
