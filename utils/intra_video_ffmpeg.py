8ce900e8ac701a91e894860c05d6783383f85525
Yutong Wang <55480809+hhhh1138@users.noreply.github.com>
2024-08-23 17:55:10 +0800

    Update intra_video_ffmpeg.py

--- utils/intra_video_ffmpeg.py
+++ utils/intra_video_ffmpeg.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser() 
     parser.add_argument("--input_video_path", type=str)
     parser.add_argument("--output_video_path", type=str)
+    args = parser.parse_args()
 
     cmd = 'ffmpeg -i {} -c:v libx264 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -x264-params "keyint=1:min-keyint=1:scenecut=0" -c:a copy {}'.format(args.input_video_path, args.output_video_path)
     os.system(cmd)
