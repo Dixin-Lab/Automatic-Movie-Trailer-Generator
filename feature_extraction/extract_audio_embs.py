5c600661294488666603b69f36af286f54c86e60
Yutong Wang <55480809+hhhh1138@users.noreply.github.com>
2024-08-23 16:33:01 +0800

    Update extract_audio_embs.py

--- feature_extraction/extract_audio_embs.py
+++ feature_extraction/extract_audio_embs.py
@@ -5,28 +5,30 @@ from imagebind.models.imagebind_model import ModalityType
 import numpy as np
 import time
 import os
+import os.path as osp 
 import math
+import argparse 
 
-device = "cuda:5" if torch.cuda.is_available() else "cpu"
-# Instantiate model
-model = imagebind_model.imagebind_huge(pretrained=True)
-model.eval()
-model.to(device)
-# Load data
 
-base = ''  # data directory that contains audio shots 
-movfiles = sorted(os.listdir(base), key=lambda x: int(str(x)))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser() 
+    parser.add_argument("--save_bar_base", type=str default='./seg_audio_shots')
+    parser.add_argument("--save_audio_embs_dir", type=str, default='./audio_embs') 
 
-save_emb_base = ""
+    os.makedirs(args.save_audio_embs_dir, exist_ok=True) 
+    audio_name = args.save_audio_base.split('/')[-1] 
+    save_embs_path = osp.join(args.save_audio_embs_dir, audio_name+'.npy')
 
-for idx in movfiles:
-    # e.g., idx: 1-1
-    movidx = str(idx)
-    print(movidx)
+    print(f'Start encoding audio embeddings.')
 
-    audio_base = os.path.join(base, '{}'.format(movidx))
-    audio_files = sorted(os.listdir(audio_base), key=lambda x: int(str(x).split('.')[0]))
-    audio_paths = [os.path.join(audio_base, file) for file in audio_files]
+    # Instantiate model
+    device = 'cuda'
+    model = imagebind_model.imagebind_huge(pretrained=True)
+    model.eval()
+    model.to(device) 
+
+    audio_files = sorted(os.listdir(args.save_bar_base), key=lambda x: int(str(x).split('.')[0]))
+    audio_paths = [os.path.join(args.save_bar_base, file) for file in audio_files] 
 
     inputs = {
         ModalityType.AUDIO: data.load_and_transform_audio_data(audio_paths, device),
@@ -34,9 +36,9 @@ for idx in movfiles:
 
     with torch.no_grad():
         embeddings = model(inputs)
-
     audio_emb = embeddings[ModalityType.AUDIO]
-    print(audio_emb.size())
     np_emb = audio_emb.detach().cpu().numpy()
-    save_path = '{}.npy'.format(movidx)
-    np.save(os.path.join(save_emb_base, save_path), np_emb)
+    np.save(save_embs_path, np_emb)
+
+    print(f'Audio bars in {args.save_bar_base} are encoded successfully. The audio embs\'shape is {audio_emb.size()}')
+
