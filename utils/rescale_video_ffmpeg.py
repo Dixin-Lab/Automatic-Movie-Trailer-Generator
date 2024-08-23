19ebc9d2f979bf95c2a51fff8219f26e7f3ab59e
Yutong Wang <55480809+hhhh1138@users.noreply.github.com>
2024-08-23 15:52:25 +0800

    Update and rename rescale_movies_ffmpeg.py to rescale_video_ffmpeg.py

---
+++ utils/rescale_video_ffmpeg.py
@@ -0,0 +1,12 @@
+import os 
+import argparse 
+
+# Rescale input video to 320p through ffmpeg
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser() 
+    parser.add_argument("--input_video_path", type=str)
+    parser.add_argument("--output_video_path", type=str)
+
+    cmd = 'ffmpeg -i {} -vf scale=320:-2 {} -hide_banner'.format(args.input_video_path, args.output_video_path)
+    os.system(cmd)
