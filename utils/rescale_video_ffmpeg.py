06cb1758fbe1ccbf62cba6af12a5447d7a76d261
Yutong Wang <55480809+hhhh1138@users.noreply.github.com>
2024-08-23 17:55:00 +0800

    Update rescale_video_ffmpeg.py

--- utils/rescale_video_ffmpeg.py
+++ utils/rescale_video_ffmpeg.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser() 
     parser.add_argument("--input_video_path", type=str)
     parser.add_argument("--output_video_path", type=str)
+    args = parser.parse_args()
 
     cmd = 'ffmpeg -i {} -vf scale=320:-2 {} -hide_banner'.format(args.input_video_path, args.output_video_path)
     os.system(cmd)
